cb153d4dc0727587d55775023fc559d735c399fb
ColAntoine <antoine.colin@epitech.eu>
2025-09-22 12:05:32 +0200

    [FEAT]: a string example for testing

--- tests/example.cpp
+++ tests/example.cpp
@@ -1,6 +1,6 @@
-// main.cpp
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
+#include "string.h"
 
 int factorial(int n) {
     return n <= 1 ? 1 : n * factorial(n - 1);
@@ -11,4 +11,12 @@ TEST_CASE("testing the factorial function") {
     CHECK(factorial(2) == 2);
     CHECK(factorial(3) == 6);
     CHECK(factorial(10) == 3628800);
+}
+
+std::string addReturnToLine(std::string str) {
+    return str + "\n";
+}
+
+TEST_CASE("test the Return to line function") {
+    CHECK(addReturnToLine("hello") == "hello\n");
 }
