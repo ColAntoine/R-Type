0a7103556e7b8c7e0928036ce3f1d6e8b5a840d5
sumerinsuyu <paul1.emeriau@epitech.eu>
2025-10-20 12:26:14 +0200

    (RTP-58/feat/Second-Game-to-show-that-our-ECS-is-independant) : [Feat] - three type of balls

--- Games/Pang/Entity/Components/Ball/Ball.cpp
+++ Games/Pang/Entity/Components/Ball/Ball.cpp
@@ -10,24 +10,29 @@
 #include "Entity/Components/Gravity/Gravity.hpp"
 
 Ball::Ball() {}
-Ball::Ball(float radius, Color color, bool isACtive)
-: _radius(radius), _color(color), _isActive(isACtive)
-{}
+Ball::Ball(float radius, Color color, bool isACtive, ballType type)
+: _radius(radius), _color(color), _isActive(isACtive), _type(type)
+{
+    if (type == CUSTOM) return;
 
-void Ball::spawn(IComponentFactory *factory, registry &r, position pos)
+    if (type == SMALL) {
+        _radius = 25.f;
+    } else if (type == MEDIUM) {
+        _radius = 50.f;
+    } else if (type == LARGE) {
+        _radius = 100.f;
+    }
+}
+
+void Ball::spawn(IComponentFactory *factory, registry &r, position pos, ballType type)
 {
     auto ballEntity = r.spawn_entity();
     factory->create_component<position>(r, ballEntity, pos.x, pos.y);
     factory->create_component<velocity>(r, ballEntity, 0.0f, 0.0f);
-    factory->create_component<Ball>(r, ballEntity, 20.0f, RED, true);
-    
-    // Create collider with proper size based on ball radius
-    // Width and height should be diameter (2 * radius)
-    // Offset to center the collider on the ball position
-    float diameter = 20.0f * 2.0f; // 2 * radius
-    factory->create_component<collider>(r, ballEntity, 
-        diameter, diameter,           // width, height (diameter)
-        -20.0f, -20.0f);             // offset to center (-radius, -radius)
-    
+    factory->create_component<Ball>(r, ballEntity, 20.0f, RED, true, type);
+    float diameter = 20.0f * 2.0f;
+    factory->create_component<collider>(r, ballEntity,
+        diameter, diameter,
+        -20.0f, -20.0f);
     factory->create_component<Gravity>(r, ballEntity, GRAVITY);
 }
--- Games/Pang/Entity/Components/Ball/Ball.hpp
+++ Games/Pang/Entity/Components/Ball/Ball.hpp
@@ -13,14 +13,22 @@
 #include "Constants.hpp"
 #include "ECS/Components/Position.hpp"
 
+enum ballType {
+    SMALL,
+    MEDIUM,
+    LARGE,
+    CUSTOM
+};
+
 struct Ball {
     float _radius{2.f};
     Color _color{Color(255, 255, 255, 255)};
     bool _isActive{true};
+    ballType _type{MEDIUM};
 
     Ball();
-    Ball(float radius, Color color, bool isACtive);
+    Ball(float radius, Color color, bool isACtive, ballType type);
 
     public:
-        void spawn(IComponentFactory *factory, registry &r, position pos);
+        void spawn(IComponentFactory *factory, registry &r, position pos, ballType type=MEDIUM);
 };
