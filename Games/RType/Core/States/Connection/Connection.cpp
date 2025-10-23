e72c58cb2b07147bb908488baf18848f9088145c
sumerinsuyu <paul1.emeriau@epitech.eu>
2025-10-23 22:29:38 +0200

    (RTP-63/feat/UI-scenes-Menu-settings-lobby) : [Feat] - port and ip input added

--- Games/RType/Core/States/Connection/Connection.cpp
+++ Games/RType/Core/States/Connection/Connection.cpp
@@ -4,6 +4,7 @@
 #include "Constants.hpp"
 #include "ECS/UI/UIBuilder.hpp"
 #include "ECS/Renderer/RenderManager.hpp"
+#include "ECS/UI/Components/InputField.hpp"
 
 void Connection::enter()
 {
@@ -66,4 +67,57 @@ void Connection::setup_ui()
 
     auto barEntity = _registry.spawn_entity();
     _registry.add_component(barEntity, UI::UIComponent(bar));
+
+    // Left side: Input fields for IP and Port
+    // IP Label
+    auto ipLabel = TextBuilder()
+        .at(renderManager.scalePosX(10), renderManager.scalePosY(13))
+        .text("IP Address:")
+        .fontSize(renderManager.scaleSizeW(3))
+        .textColor(WHITE)
+        .build(winInfos.getWidth(), winInfos.getHeight());
+
+    auto ipLabelEntity = _registry.spawn_entity();
+    _registry.add_component(ipLabelEntity, UI::UIComponent(ipLabel));
+
+    // IP Input Field
+    auto ipInput = InputBuilder()
+        .at(renderManager.scalePosX(10), renderManager.scalePosY(18))
+        .size(renderManager.scaleSizeW(20), renderManager.scaleSizeH(6))
+        .placeholder("Enter IP address")
+        .backgroundColor(Color{50, 50, 50, 255})
+        .textColor(WHITE)
+        .placeholderColor(Color{150, 150, 150, 255})
+        .border(2, Color{200, 200, 200, 255})
+        .fontSize(renderManager.scaleSizeW(2))
+        .build(winInfos.getWidth(), winInfos.getHeight());
+
+    auto ipInputEntity = _registry.spawn_entity();
+    _registry.add_component(ipInputEntity, UI::UIComponent(ipInput));
+
+    // Port Label
+    auto portLabel = TextBuilder()
+        .at(renderManager.scalePosX(10), renderManager.scalePosY(28))
+        .text("Port:")
+        .fontSize(renderManager.scaleSizeW(3))
+        .textColor(WHITE)
+        .build(winInfos.getWidth(), winInfos.getHeight());
+
+    auto portLabelEntity = _registry.spawn_entity();
+    _registry.add_component(portLabelEntity, UI::UIComponent(portLabel));
+
+    // Port Input Field
+    auto portInput = InputBuilder()
+        .at(renderManager.scalePosX(10), renderManager.scalePosY(33))
+        .size(renderManager.scaleSizeW(20), renderManager.scaleSizeH(6))
+        .placeholder("Enter port")
+        .backgroundColor(Color{50, 50, 50, 255})
+        .textColor(WHITE)
+        .placeholderColor(Color{150, 150, 150, 255})
+        .border(2, Color{200, 200, 200, 255})
+        .fontSize(renderManager.scaleSizeW(2))
+        .build(winInfos.getWidth(), winInfos.getHeight());
+
+    auto portInputEntity = _registry.spawn_entity();
+    _registry.add_component(portInputEntity, UI::UIComponent(portInput));
 }
