466756dcbc77147acac24789cc906024452ab752
sumerinsuyu <paul1.emeriau@epitech.eu>
2025-10-27 14:57:45 +0100

    (RTP-65-Game-design-and-mechanics-upgrade) : [Feat] - powerUp components implementation

---
+++ Games/RType/Entity/Components/PowerUp/PowerUp.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** PowerUp component implementation
+*/
+
+#include "PowerUp.hpp"
+
+PowerUp::PowerUp() {}
+PowerUp::PowerUp(powerUpType pwType)
+: _pwType(pwType)
+{}
