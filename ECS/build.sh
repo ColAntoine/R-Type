ae74a0bc094f025fdc5116a5c39cdcde29c521ab
gabriel-thuus <gabriel.thuus@epitech.eu>
2025-10-30 12:15:55 +0100

    (RTP-74/Refacto/CMake-to-build-every-parts-standalone) : [FEAT] - ECS build standalone with vcpkg

--- ECS/build.sh
+++ ECS/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mkdir -p build && cd build && cmake .. -DCMAKE_C_FLAGS="-fPIC" -DCMAKE_CXX_FLAGS="-fPIC" -DBUILD_SHARED_LIBS=ON && make
+mkdir -p build && cd build && cmake .. && make
